f8c4f190cc2e9a88246bdede4f316669dc862dd9
MaestroPanel <ping@maestropanel.com>
2012-02-14 15:46:23 +0200

    Commit

---
+++ maestropanel.php
@@ -0,0 +1,124 @@
+<?php
+//////////////////////////////////////////////////////////////////////////
+// Maestro Panel WHMCS API v1.0 by Smyrna Telekom
+// http://www.maestropanel.com/ - MaestroPanel
+// http://wiki.maestropanel.com/whmcs-module.ashx Help Page
+// http://www.smyrna.com.tr/ - API Developer
+// Maestro Panel WHMCS API v1.0 by Smyrna Telekom
+///////////////////////////////////////////////////////////////////////////
+  function maestro_configoptions ()
+  {
+    $configarray = array ('Domain Plan' => array ('Type' => 'text', 'Size' => '25'), 'Domain Plan Name' => array ('Type' => 'text', 'Size' => '25'));
+    return $configarray;
+  }
+  
+    function maestro_clientarea ($params)
+  {
+    $code = '<form action="http://' . $params['serverip'] . ':9715" method="get" target="_blank"><input type="submit" value="Login to Maestro Panel" class="button"></form>';
+    return $code;
+  }
+
+  function maestro_adminlink ($params)
+  {
+    $code = '<form action="http://' . $params['serverip'] . ':9715" method="get" target="_blank"><input type="submit" value="Maestro Panel"></form>';
+    return $code;
+  }
+  
+    function maestro_createaccount ($params)
+  {
+	 
+	$query3 = 'UPDATE tblhosting SET domain=\'' . $params['domain'] . '\',username=\'' . $params['domain'] . '\' WHERE id=\'' . $params['accountid'] . '\'';
+    $result3 = mysql_query ($query3);
+	  $module = 'Domain/Create';
+	  $packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'&planAlias='.$params['configoption2'].'&username='.$params['domain'].'&password='.$params['password'].'&firstname='.$params['clientsdetails']['firstname'].'&lastname='.$params['clientsdetails']['lastname'].'&email='.$params['clientsdetails']['email'].'&activedomainuser=true';
+      $retval = maestro_connection ($params, $module, $packet);
+	 if($retval['RESULT']['CODE'] != 0){
+		 $result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];  
+	  } else {
+		$result = "success";    
+	  }
+
+	return $result;
+  }
+  
+      function maestro_changepassword ($params)
+  {
+	  $module = 'Domain/Password';
+	  $packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'&newpassword='.$params['password'].'';
+      $retval = maestro_connection ($params, $module, $packet);
+	 if($retval['RESULT']['CODE'] != 0){
+		 $result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];  
+	  } else {
+		$result = "success";
+	  }
+
+	return $result;
+  }
+    function maestro_terminateaccount ($params)
+  {
+	  $module = 'Domain/Delete';
+	  $packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'';
+      $retval = maestro_connection ($params, $module, $packet);
+	 if($retval['RESULT']['CODE'] != 0){
+		 $result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];  
+	  } else {
+		$result = "success";    
+	  }
+
+	return $result;
+  }
+      function maestro_suspendaccount ($params)
+  {
+	  $module = 'Domain/Stop';
+	  $packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'';
+      $retval = maestro_connection ($params, $module, $packet);
+	 if($retval['RESULT']['CODE'] != 0){
+		 $result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];  
+	  } else {
+		$result = "success";
+	  }
+	  
+	return $result;
+  }
+      function maestro_unsuspendaccount ($params)
+  {
+	  $module = 'Domain/Start';
+	  $packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'';
+      $retval = maestro_connection ($params, $module, $packet);
+	 if($retval['RESULT']['CODE'] != 0){
+		 $result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];  
+	  } else {
+		$result = "success";    
+	  }
+
+	return $result;
+  }
+    function maestro_connection ($params, $module, $packet)
+  {
+    global $debug_output;
+    global $clientid;
+    $url = 'http://' . $params['serverip'] . ':9715/Api/'.$module;
+    $ch = curl_init ();
+    curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt ($ch, CURLOPT_URL, $url);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt ($ch, CURLOPT_TIMEOUT, 15);
+	if($module == 'Domain/Delete'){
+	curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+	} 
+	curl_setopt ($ch, CURLOPT_POSTFIELDS, $packet);
+
+    $retval = curl_exec ($ch);
+    if (curl_errno ($ch))
+    {
+      if ($debug_output == 'on')
+      {
+        echo '<textarea rows=2 cols=80>' . curl_errno ($ch) . ' - ' . curl_error ($ch) . '</textarea>';
+      }
+    }
+    curl_close ($ch);
+	$res = xmltoarray($retval);
+    return $res;
+  }
+?>
