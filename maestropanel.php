ccd961c4d37b3f00bb68982b4b377fbba0ba04b8
maestropanel <ping@maestropanel.com>
2013-02-19 17:54:18 +0200

    Reseller Support, SignOn Support, Bux Fix

--- maestropanel.php
+++ maestropanel.php
@@ -4,7 +4,7 @@
 // http://www.maestropanel.com/ - MaestroPanel
 // http://wiki.maestropanel.com/whmcs-module.ashx Help Page
 // http://www.smyrna.com.tr/ - API Developer
-// Maestro Panel WHMCS API v1.0 by Smyrna Telekom
+// Maestro Panel WHMCS API v2.0 by Smyrna Telekom
 ///////////////////////////////////////////////////////////////////////////
   function maestropanel_configoptions ()
   {
@@ -14,84 +14,107 @@
   
     function maestropanel_clientarea ($params)
   {
-    $code = '<form action="http://' . $params['serverip'] . ':9715" method="get" target="_blank"><input type="submit" value="Login to MaestroPanel" class="button"></form>';
+    $code = '<form action="http://' . $params['serverip'] . ':9715/Auth/SignOn" method="post" target="_blank"><input type="hidden" name="username" value="' . $params['username'] . '"><input type="hidden" name="password" value="' . $params['password'] . '"><input type="submit" value="Login to MaestroPanel" class="button"></form>';
     return $code;
   }
 
   function maestropanel_adminlink ($params)
   {
-    $code = '<form action="http://' . $params['serverip'] . ':9715" method="get" target="_blank"><input type="submit" value="Maestro Panel"></form>';
+    $code = '<form action="http://' . $params['serverip'] . ':9715/Auth/SignOn" method="get" target="_blank"><input type="hidden" name="username" value="' . $params['serverusername'] . '"><input type="hidden" name="password" value="' . $params['serverpassword'] . '"><input type="submit" value="Maestro Panel"></form>';
     return $code;
   }
   
     function maestropanel_createaccount ($params)
   {
-	 
-	$query3 = 'UPDATE tblhosting SET domain=\'' . $params['domain'] . '\',username=\'' . $params['domain'] . '\' WHERE id=\'' . $params['accountid'] . '\'';
+$query3 = 'UPDATE tblhosting SET domain=\'' . $params['domain'] . '\',username=\'' . $params['domain'] . '\' WHERE id=\'' . $params['accountid'] . '\'';
     $result3 = mysql_query ($query3);
-	  $module = 'Domain/Create';
-	  $packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'&planAlias='.$params['configoption2'].'&username='.$params['domain'].'&password='.$params['password'].'&firstname='.$params['clientsdetails']['firstname'].'&lastname='.$params['clientsdetails']['lastname'].'&email='.$params['clientsdetails']['email'].'&activedomainuser=true';
-      $retval = maestropanel_connection ($params, $module, $packet);
-	 if($retval['RESULT']['CODE'] != 0){
-		 $result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];  
-	  } else {
-		$result = "success";    
-	  }
+if($params['type'] == 'reselleraccount'){
+	
+$module = 'Reseller/Create';
+$packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'&planAlias='.$params['configoption2'].'&username='.$params['domain'].'&password='.$params['password'].'&firstname='.$params['clientsdetails']['firstname'].'&lastname='.$params['clientsdetails']['lastname'].'&email='.$params['clientsdetails']['email'].'&activedomainuser=true';
+} else {
 
-	return $result;
+
+$module = 'Domain/Create';
+$packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'&planAlias='.$params['configoption2'].'&username='.$params['domain'].'&password='.$params['password'].'&firstname='.$params['clientsdetails']['firstname'].'&lastname='.$params['clientsdetails']['lastname'].'&email='.$params['clientsdetails']['email'].'&activedomainuser=true';
+}
+      $retval = maestropanel_connection ($params, $module, $packet);
+if($retval['RESULT']['CODE'] != 0){
+$result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];
+} else {
+$result = "success";
+}
+
+return $result;
   }
   
       function maestropanel_changepassword ($params)
   {
-	  $module = 'Domain/Password';
-	  $packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'&newpassword='.$params['password'].'';
+if($params['type'] == 'reselleraccount'){
+$module = 'Reseller/ChangePassword'; }
+else {
+$module = 'Domain/Password';
+}
+$packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'&newpassword='.$params['password'].'';
       $retval = maestropanel_connection ($params, $module, $packet);
-	 if($retval['RESULT']['CODE'] != 0){
-		 $result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];  
-	  } else {
-		$result = "success";
-	  }
+if($retval['RESULT']['CODE'] != 0){
+$result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];
+} else {
+$result = "success";
+}
 
-	return $result;
+return $result;
   }
     function maestropanel_terminateaccount ($params)
   {
-	  $module = 'Domain/Delete';
-	  $packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'';
+if($params['type'] == 'reselleraccount'){
+$module = 'Reseller/Stop'; }
+else {
+$module = 'Domain/Stop';
+}
+$packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'';
       $retval = maestropanel_connection ($params, $module, $packet);
-	 if($retval['RESULT']['CODE'] != 0){
-		 $result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];  
-	  } else {
-		$result = "success";    
-	  }
+if($retval['RESULT']['CODE'] != 0){
+$result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];
+} else {
+$result = "success";
+}
 
-	return $result;
+return $result;
   }
       function maestropanel_suspendaccount ($params)
   {
-	  $module = 'Domain/Stop';
-	  $packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'';
+if($params['type'] == 'reselleraccount'){
+$module = 'Reseller/Stop'; }
+else {
+$module = 'Domain/Stop';
+}
+$packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'';
       $retval = maestropanel_connection ($params, $module, $packet);
-	 if($retval['RESULT']['CODE'] != 0){
-		 $result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];  
-	  } else {
-		$result = "success";
-	  }
-	  
-	return $result;
+if($retval['RESULT']['CODE'] != 0){
+$result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];
+} else {
+$result = "success";
+}
+
+return $result;
   }
       function maestropanel_unsuspendaccount ($params)
   {
-	  $module = 'Domain/Start';
-	  $packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'';
+if($params['type'] == 'reselleraccount'){
+$module = 'Reseller/Start'; }
+else {
+$module = 'Domain/Start';
+}
+$packet = 'key='.$params['serveraccesshash'].'&name='.$params['domain'].'';
       $retval = maestropanel_connection ($params, $module, $packet);
-	 if($retval['RESULT']['CODE'] != 0){
-		 $result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];  
-	  } else {
-		$result = "success";    
-	  }
+if($retval['RESULT']['CODE'] != 0){
+$result = $retval['RESULT']['CODE'] . ' - ' . $retval['RESULT']['MESSAGE'];
+} else {
+$result = "success";
+}
 
-	return $result;
+return $result;
   }
     function maestropanel_connection ($params, $module, $packet)
   {
@@ -104,10 +127,10 @@
     curl_setopt ($ch, CURLOPT_URL, $url);
     curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt ($ch, CURLOPT_TIMEOUT, 15);
-	if($module == 'Domain/Delete'){
-	curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-	} 
-	curl_setopt ($ch, CURLOPT_POSTFIELDS, $packet);
+if($module == 'Domain/Delete'){
+curl_setopt ($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+}
+curl_setopt ($ch, CURLOPT_POSTFIELDS, $packet);
 
     $retval = curl_exec ($ch);
     if (curl_errno ($ch))
@@ -118,7 +141,7 @@
       }
     }
     curl_close ($ch);
-	$res = xmltoarray($retval);
+$res = xmltoarray($retval);
     return $res;
   }
 ?>
